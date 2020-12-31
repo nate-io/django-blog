60cb0deced2f01b4ad8666f03361900db8246884
nate <nyoung125@gmail.com>
2020-12-31 10:37:21 -0500

    add dynamically rendered templates with base (#3)

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+  {
+    'author': 'CoreyMS',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'August 27, 2018'
+  },
+  {
+    'author': 'Jane Doe',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 28, 2018'
+  }
+]
 
 def home(request):
-  response = '<h1>Blog Home</h1>'
+  context = {
+    'posts': posts
+  }
 
-  return HttpResponse(response)
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  response = '<h1>Blog About</h1>'
-
-  return HttpResponse(response) 
+  context = {
+    'title': 'About'
+  }
+  
+  return render(request, 'blog/about.html', context)
