fec0eaa73e1c4018b46f26c64dd45a49a8f01860
Nathaniel Young <nyoung125@gmail.com>
2021-01-05 16:11:23 -0500

    remove old home function based view

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,6 @@ from django.views.generic import DetailView, ListView
 from .models import Post
 
 
-def home(request):
-  context = {
-    'posts': Post.objects.all()
-  }
-
-  return render(request, 'blog/home.html', context)
-
-
 class PostListView(ListView):
   model = Post
   template_name = 'blog/home.html'
