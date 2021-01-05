2c90c17a2957f43cbcc217c6cf862f7005654406
Nathaniel Young <nyoung125@gmail.com>
2021-01-05 13:53:17 -0500

    add ability to update user profile

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,3 +10,17 @@ class Profile(models.Model):
 
   def __str__(self):
     return f'{self.user.username} Profile'
+
+  # override default save behavior
+  def save(self):
+    super().save()
+
+    # max image size on site is ~125px
+    # resize to store file efficiently
+    # overwriting original image location
+    img = Image.open(self.image.path)
+
+    if img.height > 300 or img.width > 300:
+      output_size = (300, 300)
+      img.thumbnail(output_size)
+      img.save(self.image.path)
