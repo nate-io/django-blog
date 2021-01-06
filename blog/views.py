b0ad05281e837a00dd150b14fce3099a382613bf
nate <nyoung125@gmail.com>
2021-01-06 15:10:00 -0500

    Pagination (#9)
    * add mock data, fix default image location
    
    * add pagination to home page
    
    * add post list filtered by user, site links
    
    * add data load readme section

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
   CreateView, 
   DetailView,
@@ -9,18 +10,31 @@ from django.views.generic import (
 )
 from .models import Post
 
-
+# retrieve a paginated list of posts
 class PostListView(ListView):
   model = Post
   template_name = 'blog/home.html'
   context_object_name = 'posts'
   ordering = [ '-date_posted' ]
+  paginate_by = 5
 
+# retrieve a paginated list of posts filtered by user
+class UserPostListView(ListView):
+  model = Post
+  template_name = 'blog/user_posts.html'
+  context_object_name = 'posts'
+  paginate_by = 5
 
+  def get_queryset(self):
+    # grab username from url params otherwise return a 404
+    user = get_object_or_404(User, username=self.kwargs.get('username'))
+    return Post.objects.filter(author=user).order_by('-date_posted')
+
+# retrieve a specific post
 class PostDetailView(DetailView):
   model = Post
 
-
+# create a new post 
 class PostCreateView(LoginRequiredMixin, CreateView):
   model = Post
   fields = [ 'title', 'content' ]
@@ -30,7 +44,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     form.instance.author = self.request.user
     return super().form_valid(form)
 
-
+# update an existing post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
   model = Post
   fields = [ 'title', 'content' ]
@@ -47,6 +61,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
       return True
     return False
 
+# delete an existing post
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
   model = Post
   success_url = '/'
--- users/models.py
+++ users/models.py
@@ -5,8 +5,7 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
-  image_dir = 'profile_pics'
-  image = models.ImageField(default=f'/{image_dir}/default.jpg', upload_to=image_dir)
+  image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
   def __str__(self):
     return f'{self.user.username} Profile'
