f6a784c987a14d536cee2bb533da44d1c9c2de0c
Nathaniel Young <nyoung125@gmail.com>
2021-01-06 08:29:45 -0500

    convert new post ui to protected route

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,11 @@ from .views import (
     PostCreateView
 )
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/new/', login_required(PostCreateView.as_view()), name='post-create'),
     path('about/', views.about, name='blog-about')
 ]
